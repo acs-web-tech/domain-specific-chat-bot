da09088631e2e590b35dc671c7e16f248f7b2525
P.ARUN <95969896+acs-web-tech@users.noreply.github.com>
2024-06-10 08:38:29 +0530

    content correction,brand logo

--- dscb/src/Components/Connect.js
+++ dscb/src/Components/Connect.js
@@ -4,7 +4,7 @@ export default function Connect(props){
         <div className="custom-connect py-10 px-5 md:py-0 md:px-0 flex flex-col justify-center items-center w-full h-full  [font-family:Poppins]">
             <h1 className="text-white text-center text-[2.0625rem]  md:text-[4.0625rem] md:font-[600] leading-[normal] tracking-[0.12188rem]">Connect</h1>
             <p className="md:mt-3 md:w-[44.25rem] text-[1.125rem] md:text-[1.3125] md:font-[400] tracking-[0.03938rem] text-center">
-            Lorem ipsum dolor sit amet consectetur. Imperdiet convallis blandit felis ligula aliquam venenatis fghh hgjj nisi ante.
+            Confused about pricing feel free to reach us for any queries, Our experts are ready to have chat with you!,Thanks for visiting
             </p>
             <button className="mt-5 flex items-center justify-center bg-white w-[20.3125rem] md:w-[20.3125rem] md:h-[4.5625rem] md:font-[600] heading-[1.5rem] text-[#0066C8] text-[1.8rem] md:text-[1.6875rem] rounded-lg md:mt-3">Let's Connect <img  src={props.buttonIcon}/></button>
         </div>
--- dscb/src/Components/Footer.js
+++ dscb/src/Components/Footer.js
@@ -1,15 +1,16 @@
 import React from "react";
-import brand from "../static/brand-logo.svg"
 import { Link } from "react-router-dom";
 import facebook from "../static/Facebook.svg"
 import instagram from "../static/Instagram.svg"
 import LinkedIn from "../static/LinkedIn.svg"
+import logo from "../static/brand.svg"
 export default function Footer(props){
     return(
         <div className="[font-family:'Poppins'] flex flex-col relative justify-center items-center md:h-[30.5rem] md:gap-16 pt-5 md:pt-0 pb-10 md:pb-0 ">
             <div className="flex items-center">
-                <h1 className="flex justify-center items-center p-5 w-[2.625rem] h-[3.2955rem] text-[2.594rem] font-[700] bg-[#FF006B]">M</h1>
-               <h1 class="text-[2.80794rem] font-[700]">uhammad</h1>
+                {/* <h1 className="flex justify-center items-center p-5 w-[2.625rem] h-[3.2955rem] text-[2.594rem] font-[700] bg-[#FF006B]">M</h1> */}
+                <h1 className=""><img src={logo}/></h1>
+               <h1 class="ml-3 text-[2.80794rem] font-[700]">4DUCATE</h1>
             </div>
             <section className="flex flex-col justify-center gap-3 md:flex-row items-center md:justify-start md:items-start md:flex-row md:gap-5">
                <div className="text-[1.125rem] font-400 tracking-[0.03938rem]"><Link>Home</Link></div>
@@ -24,7 +25,7 @@ export default function Footer(props){
                <div className="md:text-[1.125rem] md:font-400 md:tracking-[0.03938rem]"><Link><img src={instagram}/></Link></div>
                <div className="md:text-[1.125rem] md:font-400 md:tracking-[0.03938rem]"><Link><img src={LinkedIn}/></Link></div>
             </section>
-            <div className="w-full text-black self-end bg-white absolute bottom-0 text-center md:text-[1.3125rem] md:font-[400] tracking-[0.03938rem]">Powered By 4DUCATE</div>
+            <div className="w-full text-black self-end bg-white absolute bottom-0 text-center md:text-[1.3125rem] md:font-[400] tracking-[0.03938rem]">Distributed By 4DUCATE</div>
         </div>
     )
 }
